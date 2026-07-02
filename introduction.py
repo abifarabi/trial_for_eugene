c79dfcc5426bf910952198051c524290957232f2
Azali Farabi <157711273+abifarabi@users.noreply.github.com>
2026-07-02 09:46:56 +0700

    Add introduction functions for user greeting
    Hi there this is just a simple introduction from Eugene. Hope you like it and don't judge!

---
+++ introduction.py
@@ -0,0 +1,7 @@
+def intro_eugene(name, course):
+  print("Hello there my name is", name)
+  print("I join this", course, "to improve my coding skill")
+
+def loop_intro(n):
+  for i in range(n):
+    print(i+1, "I will be a great developer")
